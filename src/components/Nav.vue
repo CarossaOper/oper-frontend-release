6f8b51a5cf24583e1c6a87f7ac53070051031192
Jakob Reiser <jakobreiser@gmx.de>
2022-02-12 14:29:17 +0100

    Some navbar improvements (main focus: mobile view)

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,11 +1,17 @@
 <template>
     <nav class="navbar navbar-expand-lg">
-        <div class="container-fluid">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <div class="container-fluid d-flex justify-content-end">
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="fa fa-bars fa-2x" aria-hidden="true" style="color: white;"></span>
             </button>
 
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasRight" aria-labelledby="offcanvasRightLabel">
+            <div class="offcanvas-header">
+                <button class="navbar-toggler me-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="fa fa-solid fa-xmark fa-2x" aria-hidden="true" style="color: white;"></span>
+                </button>
+            </div>
+            <div class="offcanvas-body">
                 <ul class="navbar-nav ms-auto mb-4 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" href="">
@@ -32,6 +38,7 @@
                     </li>
                 </ul>
             </div>
+            </div>
         </div>
     </nav>
 </template>
@@ -45,6 +52,8 @@ export default {
 <style scoped>
 
 .navbar {
+    text-transform: uppercase;
+    font-weight: 100;
     font-family: 'Oswald', sans-serif;
 }
 
@@ -78,5 +87,12 @@ nav .navbar-nav li a:after {
 
 nav .navbar-nav li a:hover:after {
     width: 100%;
+}
+
+/*-------Mobile navbar--------*/
+@media only screen and (max-width: 991.5px) {
+    .offcanvas {
+        background-color: rgb(0, 0, 0) !important;
+    }
 }
 </style>
