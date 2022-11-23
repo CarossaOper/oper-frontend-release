504793350ec4f9bdd9f1a669f10605449330f71e
Jakob Reiser <jakobreiser@gmx.de>
2022-11-23 19:47:29 +0100

    Fix background image layout (no repeat, cover)

--- layouts/hero.vue
+++ layouts/hero.vue
@@ -1,13 +1,7 @@
 <template>
-  <div class="lg:w-[90%] h-screen bg-hero mx-auto flex overflow-hidden grayscale-[0.1] -z-10 relative">
+  <div class="lg:w-[90%] h-screen bg-hero bg-no-repeat bg-cover mx-auto flex overflow-hidden grayscale-[0.1] -z-10 relative">
     <div class="mx-auto mt-[50vh] h-fit w-fit align-middle justify-center max-w-[50%]">
       <slot />
     </div>
   </div>
 </template>
-
-<style scoped>
- /*
-  border-white border-8 py-4
- */
-</style>
