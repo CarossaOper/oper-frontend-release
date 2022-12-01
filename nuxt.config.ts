ff50b52150196dd7655f93e89399ac1e4b7a1230
VMLK <louiskaran@gmail.com>
2022-12-01 18:33:34 +0100

    Added Favicons and updated time

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,7 @@ export default defineNuxtConfig({
 
     app: {
       head: {
+        title: 'Die Carossa Oper',
         meta: [
           { charset: 'utf-8' },
           { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -11,6 +12,9 @@ export default defineNuxtConfig({
         ],
         link: [
           { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+          { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
+          { rel: 'icon', sizes: '32x32', href: '/favicon-32x32.png' },
+          { rel: 'icon', sizes: '16x16', href: '/favicon-16x16.png' },
           { rel: 'preconnect', href: 'api.fonts.coollabs.io'},
           { rel: 'stylesheet', type: 'text/css', href: 'https://api.fonts.coollabs.io/css2?family=Vollkorn+SC&display=swap'},
           { rel: 'stylesheet', type: 'text/css', href: 'https://api.fonts.coollabs.io/css2?family=Noto+Serif:wght@400;700&display=swap'}
